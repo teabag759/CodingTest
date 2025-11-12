a7accb75511dfd86eb789feba0e45ee9e7e702f3
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2025-11-12 22:27:15 +0900

    [Bronze V] Title: A/B, Time: 172 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double c = (double) a / (double) b;
+
+        System.out.println(c);
+
+    }
+}
