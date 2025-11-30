83d1a10346e6dac79fe734db20ae2c3011d0e45d
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2025-11-30 23:57:48 +0900

    [Bronze V] Title: X보다 작은 수, Time: 468 ms, Memory: 31092 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < x) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+    }
+}
