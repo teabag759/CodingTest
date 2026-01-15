ae5a82765650807361e8d50a139b2ef187d29616
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2026-01-15 22:43:26 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 524 ms, Memory: 36100 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
+
+        // arr1 생성
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        // arr2 생성 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr1[i][j] + arr2[i][j] + " ");
+                if (j == m - 1)
+                    System.out.println();
+            }
+        }
+
+    }
+}
