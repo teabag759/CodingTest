1458ca1517b36657bbad2130976c87f129eb482c
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2026-03-01 23:48:59 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int index = Integer.parseInt(br.readLine());
+        br.close();
+
+        System.out.println(line.charAt(index - 1));
+
+    }
+}
