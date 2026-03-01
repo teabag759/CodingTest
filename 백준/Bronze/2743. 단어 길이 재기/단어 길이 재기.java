0aecc1a1f8c60886d0cd379b9df9d5d6abb96b10
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2026-03-01 22:52:50 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 160 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int len = sc.next().length();
+
+        System.out.println(len);
+
+    }
+}
